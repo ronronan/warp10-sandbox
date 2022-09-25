e7026510ac31344e51c78d8ee993cef9b99f2b83
Ronan MOREL <ronanmorel@outlook.fr>
2022-09-25 11:43:14 +0200

    feat: read csv file

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,40 @@
+import fs from 'fs';
+import { parse } from 'csv-parse';
 
-const test = 'toto';
+const DATA_DAILY_CONSUMPTION = './data/ma-conso-quotidienne-14459913066639-35690.csv';
+const DATA_DAILY_INDEX = './data/mes-index-elec-14459913066639-35690.csv';
 
-export function toto(): string {
-  return 'toto';
+interface IDataConsumptionIndex {
+  timestamp: number;
+  indexHC: number;
+  indexHP: number;
 }
 
-console.log(test);
+
+function readContentCSV(fileName: string): Promise<IDataConsumptionIndex[]> {
+  return new Promise((resolve, reject) => {
+    const data: IDataConsumptionIndex[] = [];
+    fs.createReadStream(fileName)
+      .pipe(parse({ delimiter: ';', from_line: 2 }))
+      .on('data', (row: string[]) => {
+        const tmpDataSplit = row[0].split('/');
+        if (tmpDataSplit.length === 3) {
+          const tmpDate = new Date(Number(tmpDataSplit[2]), (Number(tmpDataSplit[1])-1), Number(tmpDataSplit[0]));
+          data.push({
+            timestamp: tmpDate.getTime() * 1000,
+            indexHC: Number(row[2]),
+            indexHP: Number(row[3])
+          });
+        }
+      })
+      .on('end', () => resolve(data))
+      .on('error', reject);
+  });
+}
+
+
+readContentCSV(DATA_DAILY_INDEX)
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => console.error(err));
