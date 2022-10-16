9d5fff23645c53f13484fc8a54a1d31e6b3a65fb
Ronan MOREL <ronanmorel@outlook.fr>
2022-10-16 19:52:15 +0200

    feat: read livret csv file

--- src/budget/insert-data.ts
+++ src/budget/insert-data.ts
@@ -1,45 +1,52 @@
-// import fs from 'fs';
-// import { parse } from 'csv-parse';
+import fs from 'fs';
+import { parse } from 'csv-parse';
 import { Warp10Wrapper } from '../warp10.wrapper';
+
+interface IDataBudget {
+  timestamp: number;
+  label: string;
+  sum: Number;
+}
+
+function readContentLivretCSV(fileName: string): Promise<IDataBudget[]> {
+  let cpt = 0;
+  const labelTab: string[] = [];
+  return new Promise((resolve, reject) => {
+    const data: IDataBudget[] = [];
+    fs.createReadStream(fileName)
+      .pipe(parse({ delimiter: ';', from_line: 1 }))
+      .on('data', (row: string[]) => {
+        if (cpt === 0) {
+          labelTab.push(...row);
+          labelTab.splice(0, 1);
+        } else {
+          const tmpDataSplit = row[0].split('/');
+          if (tmpDataSplit.length === 3) {
+            const tmpDate = new Date(Number(tmpDataSplit[2]), (Number(tmpDataSplit[1])-1), Number(tmpDataSplit[0]));
+            if (row.length -1 === labelTab.length) {
+              for (let i = 1; i < row.length; i++) {
+                data.push({
+                  timestamp: tmpDate.getTime() * 1000,
+                  label: labelTab[i-1],
+                  sum: Number(row[i])
+                });
+              }
+            }
+          }
+        }
+        cpt++;
+      })
+      .on('end', () => resolve(data))
+      .on('error', reject);
+  });
+}
 
 module.exports = async (w10: Warp10Wrapper) => {
   console.log('Budget insert-data.ts');
-  // const DATA_DAILY_CONSUMPTION = './data/ma-conso-quotidienne-14459913066639-35690.csv';
-  // const DATA_DAILY_INDEX = './data/mes-index-elec-14459913066639-35690.csv';
+  const livretFiles = ['2020-livret.csv', '2021-livret.csv', '2022-livret.csv']
 
-  // interface IDataConsumptionIndex {
-  //   timestamp: number;
-  //   indexHC: number;
-  //   indexHP: number;
-  // }
-
-
-  // function readContentCSV(fileName: string): Promise<IDataConsumptionIndex[]> {
-  //   return new Promise((resolve, reject) => {
-  //     const data: IDataConsumptionIndex[] = [];
-  //     fs.createReadStream(fileName)
-  //       .pipe(parse({ delimiter: ';', from_line: 2 }))
-  //       .on('data', (row: string[]) => {
-  //         const tmpDataSplit = row[0].split('/');
-  //         if (tmpDataSplit.length === 3) {
-  //           const tmpDate = new Date(Number(tmpDataSplit[2]), (Number(tmpDataSplit[1])-1), Number(tmpDataSplit[0]));
-  //           data.push({
-  //             timestamp: tmpDate.getTime() * 1000,
-  //             indexHC: Number(row[2]),
-  //             indexHP: Number(row[3])
-  //           });
-  //         }
-  //       })
-  //       .on('end', () => resolve(data))
-  //       .on('error', reject);
-  //   });
-  // }
-
-
-  // readContentCSV(DATA_DAILY_INDEX)
-  //   .then((res) => {
-  //     console.log(res);
-  //   })
-  //   .catch((err) => console.error(err));
-
+  for (const livretFile of livretFiles) {
+    const result = await readContentLivretCSV(`./data/budget/${livretFile}`);
+    console.log(result);
+  }
 }
